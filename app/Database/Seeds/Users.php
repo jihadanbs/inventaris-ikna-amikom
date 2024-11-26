d89c00894cf962954620e404d1dd7062567e1871
Jihadan Beckhianosyuhada <jihadanbs11@gmail.com>
2024-11-26 13:24:45 +0700

    [DELETE] menghapus file yang tidak diperlukan

--- app/Database/Seeds/Users.php
+++ app/Database/Seeds/Users.php
@@ -11,43 +11,23 @@ class Users extends Seeder
         //menambahkan data dalam tabel user
         $data = [
             [
-                'nama_lengkap' => 'Jihadan Beckhianosyuhada',
-                'username' => 'jihadan',
+                'nama_lengkap' => 'Florensius Alanroa Cikita',
+                'username' => 'alan',
                 'id_jabatan' => '1',
                 'password' => password_hash('1234', PASSWORD_DEFAULT),
                 'status' => 'aktif',
-                'email' => 'jihadanbs11@gmail.com',
-                'no_telepon' => '088215178312',
+                'email' => 'alanflorensius390@gmail.com',
+                'no_telepon' => '081236656992',
                 'file_profil' => 'gambar.jpg'
             ],
             [
-                'nama_lengkap' => 'Muhammad Arrayan',
-                'username' => 'arrayan',
+                'nama_lengkap' => 'Temennya Alan',
+                'username' => 'admin',
                 'id_jabatan' => '1',
                 'status' => 'aktif',
                 'password' => password_hash('12345', PASSWORD_DEFAULT),
-                'email' => 'mazoelian33@gmail.com',
-                'no_telepon' => '088215178313',
-                'file_profil' => 'gambar.jpg'
-            ],
-            [
-                'nama_lengkap' => 'Halimah Mufita',
-                'username' => 'mufita',
-                'id_jabatan' => '1',
-                'status' => 'aktif',
-                'password' => password_hash('123456', PASSWORD_DEFAULT),
-                'email' => 'mufitacool@gmail.com',
-                'no_telepon' => '088215178314',
-                'file_profil' => 'gambar.jpg'
-            ],
-            [
-                'nama_lengkap' => 'Cindy Loria',
-                'username' => 'cindy',
-                'id_jabatan' => '1',
-                'status' => 'aktif',
-                'password' => password_hash('1234567', PASSWORD_DEFAULT),
-                'email' => 'cindyloria@gmail.com',
-                'no_telepon' => '088215178315',
+                'email' => 'gantisendiri@gmail.com',
+                'no_telepon' => '0812345678891',
                 'file_profil' => 'gambar.jpg'
             ],
         ];
