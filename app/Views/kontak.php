a7cbe44e37d016f27e77909faf9c050b203a7d85
arzaqqq <mmiftahularzaq01@gmail.com>
2025-01-01 23:41:33 +0700

    perbaikan halaman kontak

--- app/Views/kontak.php
+++ app/Views/kontak.php
@@ -2,37 +2,6 @@
 
 <body class="sub_page">
     <style>
-    .card {
-        transition: transform 0.3s ease, box-shadow 0.3s ease;
-    }
-
-    .card:hover {
-        transform: translateY(-10px);
-        /* Mengangkat card ke atas */
-        box-shadow: 0 10px 20px rgba(0, 0, 0, 0.2);
-        /* Efek bayangan */
-    }
-
-    .card-body {
-        transition: color 0.3s ease;
-    }
-
-    .card:hover .card-body h5 {
-        color: #007bff;
-        /* Ubah warna judul saat hover */
-    }
-
-    .card:hover svg {
-        transform: rotate(15deg);
-        /* Sedikit memutar icon saat hover */
-        transition: transform 0.3s ease;
-    }
-
-    .card:hover i {
-        transform: scale(1.2);
-        /* Perbesar icon sedikit saat hover */
-        transition: transform 0.3s ease;
-    }
     </style>
 
 
@@ -52,7 +21,7 @@
                 <div class="col-lg-8 col-md-6 mb-4">
                     <div class="card text-center shadow">
                         <div class="card-body">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="70" height="70" fill="currentColor"
+                            <svg xmlns="http://www.w3.org/2000/svg" width="55" height="55" fill="currentColor"
                                 class="bi bi-tiktok" viewBox="0 0 16 16">
                                 <path
                                     d="M9 0h1.98c.144.715.54 1.617 1.235 2.512C12.895 3.389 13.797 4 15 4v2c-1.753 0-3.07-.814-4-1.829V11a5 5 0 1 1-5-5v2a3 3 0 1 0 3 3z" />
