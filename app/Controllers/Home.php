9360617caca889ecf571539e825bcacfc9288451
Jihadan Beckhianosyuhada <jihadanbs11@gmail.com>
2025-01-13 14:13:36 +0700

    [EXPERIMENTAL] merapihkan code

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -2,37 +2,8 @@
 
 namespace App\Controllers;
 
-use App\Models\PemohonModel;
-use App\Models\WebOptionModel;
-use App\Models\FotoPengurusModel;
-use App\Models\GaleriKegiatanModel;
-use App\Models\InformasiPublikModel;
-
 class Home extends BaseController
 {
-    protected $validation;
-    protected $m_web_option;
-    protected $m_pemohon;
-    protected $m_informasi_publik;
-    protected $m_wilayah;
-    protected $m_slider;
-    protected $db;
-    protected $galeriKegiatanModel;
-
-    protected $fotoPengurusModel;
-
-    public function __construct()
-    {
-        $this->db = \Config\Database::connect();
-        $this->m_web_option = new WebOptionModel();
-        $this->m_pemohon = new PemohonModel();
-        $this->m_informasi_publik = new InformasiPublikModel();
-        $this->galeriKegiatanModel = new GaleriKegiatanModel();
-        $this->fotoPengurusModel = new FotoPengurusModel();
-    }
-
-
-
     public function index($page = 1)
     {
 
@@ -63,27 +34,6 @@ class Home extends BaseController
         return view('about', $data);
     }
 
-    public function service($page = 1)
-    {
-        $perPage = 8; // Jumlah data per halaman
-        $offset = ($page - 1) * $perPage;
-
-        // Ambil data dengan limit dan offset
-        $galeriKegiatan = $this->galeriKegiatanModel->orderBy('tanggal_foto', 'DESC')->findAll($perPage, $offset);
-        $totalData = $this->galeriKegiatanModel->countAll(); // Total data
-
-        // Kirim data ke view
-        $data = [
-            'title' => 'Galeri Kegiatan',
-            'galeriKegiatan' => $galeriKegiatan,
-            'currentPage' => $page,
-            'totalPages' => ceil($totalData / $perPage),
-        ];
-
-        return view('service', $data);
-    }
-
-
     public function galeri($page = 1)
     {
         $perPage = 8; // Jumlah data per halaman
@@ -103,7 +53,6 @@ class Home extends BaseController
 
         return view('galeri', $data);
     }
-
 
     public function kontak()
     {
