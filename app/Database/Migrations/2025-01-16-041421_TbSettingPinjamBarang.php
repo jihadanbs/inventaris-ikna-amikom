263d7f17b5162eb3bb60b0b1d65f2d707ed9aad4
Jihadan Beckhianosyuhada <jihadanbs11@gmail.com>
2025-01-16 11:17:35 +0700

    [ADD] menambahkan tabel setting pinjam barang

---
+++ app/Database/Migrations/2025-01-16-041421_TbSettingPinjamBarang.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class TbSettingPinjamBarang extends Migration
+{
+    public function up()
+    {
+        // Membuat tabel tb_setting_pinjam_barang
+        $this->forge->addField([
+            'id_setting_pinjam_barang' => [
+                'type' => 'INT',
+                'constraint' => 11,
+                'unsigned' => TRUE,
+                'auto_increment' => TRUE
+            ],
+            'id_barang' => [
+                'type' => 'INT',
+                'constraint' => 11,
+                'unsigned' => TRUE
+            ],
+            'is_tampil' => [
+                'type' => 'TINYINT',
+                'constraint' => 1,
+                'default' => 0,
+                'comment' => '0 = Tidak ditampilkan, 1 = Ditampilkan'
+            ],
+            'masa_pinjam' => [
+                'type' => 'INT',
+                'constraint' => 11,
+                'comment' => 'Durasi peminjaman dalam hari'
+            ],
+            // 'status' => [
+            //     'type' => 'ENUM',
+            //     'constraint' => ['tersedia', 'dipinjam'],
+            //     'default' => 'tersedia'
+            // ],
+            'created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP',
+            'updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'
+        ]);
+
+        $this->forge->addKey('id_setting_pinjam_barang', TRUE);
+        $this->forge->addForeignKey('id_barang', 'tb_barang', 'id_barang', 'CASCADE', 'CASCADE');
+        $this->forge->createTable('tb_setting_pinjam_barang');
+    }
+
+    public function down()
+    {
+        // Menghapus tabel tb_setting_pinjam_barang
+        $this->forge->dropTable('tb_setting_pinjam_barang');
+    }
+}
