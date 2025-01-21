40089fe0d4229e0c3bcbdd7fa7182972631e1c0d
Arzaqqq <mmiftahularzaq01@gmail.com>
2025-01-21 19:21:18 +0700

    [UPDATE] Perbaiki alert yang muncul pada form pemesanan barang dari boostrap 5 ke boostrap 4

--- app/Views/alert/alert.php
+++ app/Views/alert/alert.php
@@ -1,30 +1,39 @@
 <?php if (session()->getFlashdata('pesan')) : ?>
-    <div class="alert alert-success alert-border-left alert-dismissible fade show" role="alert">
-        <i class="mdi mdi-check-all me-3 align-middle"></i><strong>Sukses</strong> -
+    <div class="alert alert-success alert-dismissible fade show" role="alert">
+        <i class="mdi mdi-check-all mr-2 align-middle"></i><strong>Sukses</strong> - 
         <?= session()->getFlashdata('pesan') ?>
-        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+        </button>
     </div>
 <?php endif; ?>
 
 <?php if (session()->getFlashdata('gagal')) : ?>
-    <div class="alert alert-danger alert-border-left alert-dismissible fade show" role="alert">
-        <i class="mdi mdi-block-helper me-3 align-middle"></i><strong>Gagal</strong> -
+    <div class="alert alert-danger alert-dismissible fade show" role="alert">
+        <i class="mdi mdi-block-helper mr-2 align-middle"></i><strong>Gagal</strong> - 
         <?= session()->getFlashdata('gagal') ?>
-        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+        </button>
     </div>
 <?php endif; ?>
 
 <?php if (session()->getFlashdata('error')) : ?>
-    <div class="alert alert-danger alert-border-left alert-dismissible fade show" role="alert">
-        <i class="mdi mdi-block-helper me-3 align-middle"></i><strong>Gagal</strong> -
+    <div class="alert alert-danger alert-dismissible fade show" role="alert">
+        <i class="mdi mdi-block-helper mr-2 align-middle"></i><strong>Gagal</strong> - 
         <?= session()->getFlashdata('error') ?>
-        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+        </button>
     </div>
 <?php endif; ?>
+
 <?php if (session()->getFlashdata('warning')) : ?>
-    <div class="alert alert-warning alert-border-left alert-dismissible fade show" role="alert">
-        <i class="mdi mdi-alert-outline align-middle me-3"></i><strong>Peringatan</strong> -
+    <div class="alert alert-warning alert-dismissible fade show" role="alert">
+        <i class="mdi mdi-alert-outline mr-2 align-middle"></i><strong>Peringatan</strong> - 
         <?= session()->getFlashdata('warning') ?>
-        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+        </button>
     </div>
 <?php endif; ?>
