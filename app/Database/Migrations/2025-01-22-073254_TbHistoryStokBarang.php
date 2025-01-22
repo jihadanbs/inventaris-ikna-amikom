774f99ad067483b955106d46796117f44f6db945
Jihadan Beckhianosyuhada <jihadanbs11@gmail.com>
2025-01-22 18:56:20 +0700

    [ADD] menambahkan tabel history buat nambah stock barang

---
+++ app/Database/Migrations/2025-01-22-073254_TbHistoryStokBarang.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class TbHistoryStokBarang extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id_history' => [
+                'type' => 'INT',
+                'constraint' => 11,
+                'unsigned' => true,
+                'auto_increment' => true,
+            ],
+            'id_barang' => [
+                'type' => 'INT',
+                'constraint' => 11,
+                'unsigned' => true,
+            ],
+            // 'tanggal_masuk' => [
+            //     'type' => 'DATE',
+            // ],
+            'jumlah_masuk' => [
+                'type' => 'INT',
+                'constraint' => 11,
+            ],
+            // 'kondisi_masuk' => [
+            //     'type' => 'ENUM',
+            //     'constraint' => ['baik', 'rusak'],
+            // ],
+            // 'keterangan' => [
+            //     'type' => 'TEXT',
+            //     'null' => true,
+            // ],
+            'created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP',
+            'updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP',
+            'created_by' => [
+                'type' => 'INT',
+                'constraint' => 11,
+                'unsigned' => true,
+                'null' => true,
+            ],
+        ]);
+
+        $this->forge->addKey('id_history', true);
+        $this->forge->addForeignKey('id_barang', 'tb_barang', 'id_barang', 'CASCADE', 'CASCADE');
+        $this->forge->addForeignKey('created_by', 'tb_user', 'id_user', 'SET NULL', 'CASCADE');
+        $this->forge->createTable('tb_history_stock_barang');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('tb_history_stock_barang');
+    }
+}
