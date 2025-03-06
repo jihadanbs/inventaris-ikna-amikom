73351a43c999d702225b9edfd968a8e006ba8480
Arzaqqq <mmiftahularzaq01@gmail.com>
2025-03-06 12:17:03 +0700

    [UPDATE] Memperbaiki tampilan pada register karena ada css pada container tertimpa

--- app/Views/users/registrasi.php
+++ app/Views/users/registrasi.php
@@ -20,9 +20,7 @@
     }
 
     .login-container {
-        background: white;
-        border-radius: 10px;
-        box-shadow: 0 4px 10px rgba(0, 0, 0, 0.1);
+        
         display: flex;
         flex-direction: column;
         align-items: center;
@@ -97,7 +95,7 @@
             max-width: 450px;
             margin: auto;
             padding: 20px;
-            box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
+           
         }
 
         .form-container {
